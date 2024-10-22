7f2e885c71705f128fd582fbd3fd943fe1558944
Tatiana Culeac <culeactatiana1103@gmail.com>
2024-10-22 12:38:02 +0300

    js#homework7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('ul.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+        })
+        .join('');
+};
+
+const galleryHandler = (event) => {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    const originalUrl = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${originalUrl}">`)
+    instance.show();
+
+    window.addEventListener('keydown', onEscKeyPress);
+
+    function onEscKeyPress(event) {
+        const ESC_KEY_CODE = 'Escape'
+        if (event.code === ESC_KEY_CODE) {
+            instance.close();
+            window.removeEventListener('keydown', onEscKeyPress);
+        };
+    };
+};
+
+galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
+galleryContainer.addEventListener('click', galleryHandler);
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('ul.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+        })
+        .join('');
+};
+
+galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
+
+const galleryHandler = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
+galleryHandler.on('show.simplelightbox');
+
 console.log(galleryItems);
